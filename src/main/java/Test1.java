b1aaee7ed9921d58c6ccd3209e00cb7901528115
zulonghao <726461151@qq.com>
2018-03-11 18:34:34 +0800

    添加输出2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("dsfsdfsdf");
+        System.out.println("fds");
     }
 }
